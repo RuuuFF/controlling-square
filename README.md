df32d78c152ec68dbe8b7dd45939fe92418d1aad
RuuuFF <ruuufffaria@gmail.com>
2020-11-11 18:54:52 -0300

    README.md update

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Pensei nisso e me "desafiei" a fazer, até que deu certo. 😶
 - HTML 
 - CSS 
 - JavaScript
+
+## 🖥️ Frontend
+<h1 align="center">
+  <img alt="Imagem do frontend de controlling square" src="https://i.imgur.com/1iOqWCi.png" width="450px">
+</h1>
--- main.js
+++ main.js
@@ -6,21 +6,16 @@ const move = document
 const boxMove = document
   .querySelector(".box-move");
 
-
-
 // Pegando os valores das propriedades no CSS 
 // https://bit.ly/346OmvE
 var heightString = window.getComputedStyle(boxMove).height;
 var widthString = window.getComputedStyle(boxMove).width;
+// Parece não ser necessário o "window.".
 
-
-
-// Convertendo string em número 
+// Convertendo string em number 
 // https://bit.ly/346Bk13
 var heightLimit = parseFloat(heightString);
 var widthLimit = parseFloat(widthString);
-
-
 
 let moveS = move.style;
 let pxUp = 0, pxLeft = 0;
